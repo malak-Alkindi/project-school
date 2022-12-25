938740a8d48dafde5ede162f8920bfd991151017
unknown <malakalkindi1995@gmail.com>
2022-12-24 22:47:44 -0800

    finish getting the input

--- src/collage/main.java
+++ src/collage/main.java
@@ -30,7 +30,6 @@ public class main {
 					System.out.println("what is the phone number of the school ?");
 					schoolObj.setPhoneNumber(sc.nextInt());
 					sc.nextLine();
-					
 
 					int departmetIndex = 0;
 					boolean departmetFlag = true;
@@ -47,8 +46,7 @@ public class main {
 						sc.nextLine();
 						System.out.println("what is the name of the department ?");
 						departmentObj.setName(sc.nextLine());
-				
-						
+
 						int teacherIndex = 0;
 						boolean teacherFlag = true;
 						ArrayList<Teacher> teacherList = new ArrayList<>();
@@ -63,7 +61,7 @@ public class main {
 							teacherObj.setName(sc.nextLine());
 							System.out.println("what is the teacher type ?");
 							teacherObj.setType(sc.nextLine());
-							
+
 							int studentIndex = 0;
 							boolean studentFlag = true;
 							ArrayList<Student> studentList = new ArrayList<>();
@@ -73,15 +71,64 @@ public class main {
 							while (studentFlag) {
 								studentList.add(new Student());
 								teacherObj.setStudentList(studentList);
-								Student studentObj =teacherObj.getStudentList().get(studentIndex);
+								Student studentObj = teacherObj.getStudentList().get(studentIndex);
 								System.out.println("what is the studnet age ?");
 								studentObj.setAge(sc.nextInt());
 								sc.nextLine();
 								System.out.println("what is the student name ?");
 								studentObj.setName(sc.nextLine());
-								
-								
-								
+
+								int courseIndex = 0;
+								boolean courseFlag = true;
+								ArrayList<Course> courseList = new ArrayList<>();
+
+								System.out.println("          <add first course>");
+								System.out.println();
+								while (courseFlag) {
+									courseList.add(new Course());
+									studentObj.setCourseList(courseList);
+									Course courseObj = studentObj.getCourseList().get(courseIndex);
+									System.out.println("what is the course name ?");
+									courseObj.setName(sc.nextLine());
+							
+									System.out.println("what is the course id ?");
+									courseObj.setId(sc.nextInt());
+									sc.nextLine();
+
+									int markIndex = 0;
+									boolean markFlag = true;
+									ArrayList<Mark> markList = new ArrayList<>();
+
+									System.out.println("          <add first mark>");
+									System.out.println();
+									while (markFlag) {
+										markList.add(new Mark());
+									courseObj.setMarkList(markList);
+										Mark markObj = courseObj.getMarkList().get(markIndex);
+										System.out.println("what is the subject name ?");
+										markObj.setSubjectName(sc.nextLine());
+							
+										System.out.println("what is the subject mark ?");
+										markObj.setMark(sc.nextInt());
+										sc.nextLine();
+
+										System.out.println("do you want to add another mark y/n?");
+										if (sc.nextLine().equals("y")) {
+											markIndex++;
+										} else {
+											markFlag = false;
+											System.out.println("mark added");
+										}
+									}
+									System.out.println("do you want to add another course y/n?");
+									if (sc.nextLine().equals("y")) {
+										courseIndex++;
+									} else {
+										courseFlag = false;
+										System.out.println("course added");
+									}
+								}
+
 								System.out.println("do you want to add another student y/n?");
 								if (sc.nextLine().equals("y")) {
 									studentIndex++;
@@ -90,8 +137,7 @@ public class main {
 									System.out.println("student added");
 								}
 							}
-							
-							
+
 							System.out.println("do you want to add another teacher y/n?");
 							if (sc.nextLine().equals("y")) {
 								teacherIndex++;
@@ -100,7 +146,7 @@ public class main {
 								System.out.println("teadcher added");
 							}
 						}
-						
+
 						System.out.println("do you want to add another department y/n?");
 						if (sc.nextLine().equals("y")) {
 							departmetIndex++;
